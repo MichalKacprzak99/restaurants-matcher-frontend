c3118721caafd844f9d4d54561f54e25843cb012
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-12-06 20:57:01 +0100

    finish adding restaurant

--- restaurant-matcher-front-end/src/pages/RestaurantPage/components/RestaurantPopUpAddingForm/RestaurantPopUpAddingForm.js
+++ restaurant-matcher-front-end/src/pages/RestaurantPage/components/RestaurantPopUpAddingForm/RestaurantPopUpAddingForm.js
@@ -1,85 +1,127 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Dialog, DialogActions, DialogContent, MenuItem, TextField} from "@mui/material";
 import Button from "@mui/material/Button";
-import {Controller, FormProvider, useForm} from "react-hook-form";
+import {useForm} from "react-hook-form";
+import axios from "../../../../axios.config";
 
 
 const RestaurantPopUpAddingForm = ({open, setOpen, setRestaurants}) => {
-  const {handleSubmit, control, reset} = useForm({
-    defaultValues: {
-      test: "1",
-    },
-  });
+  const [cuisines, setCuisines] = useState([])
+  const [persons, setPersons] = useState([])
+
+  const {handleSubmit, reset, register} = useForm({});
+
+
+  const fetchPersons = () => {
+    axios.get(`person/`)
+      .then(res => {
+        if (res.status === 200) {
+          setPersons(res.data)
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.error(error.response.data); // => the response payload
+        }
+      });
+  }
+
+
+  const fetchCuisines = () => {
+    axios.get(`cuisine/`)
+      .then(res => {
+        if (res.status === 200) {
+          setCuisines(res.data)
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.error(error.response.data); // => the response payload
+        }
+      });
+  }
+
 
   const handleClose = () => {
     setOpen(false);
     reset()
   };
 
+
   const handleAdd = (data) => {
-    console.log(data)
-    // axios.post(`restaurant/`, data)
-    //   .then(res => {
-    //
-    //     if (res.status === 201) {
-    //       setRestaurants(restaurants => [...restaurants, data]);
-    //       handleClose()
-    //     }
-    //   })
-    //   .catch((error) => {
-    //     if (error.response) {
-    //       console.error(error.response.data); // => the response payload
-    //     }
-    //   });
+    axios.post(`restaurant/`, data)
+      .then(res => {
+
+        if (res.status === 201) {
+          setRestaurants(restaurants => [...restaurants, data]);
+          handleClose()
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.error(error.response.data); // => the response payload
+        }
+      });
   }
+
+
+  useEffect(() => {
+    fetchPersons();
+    fetchCuisines();
+  }, []);
 
   return (
     <Dialog open={open}>
       <DialogContent>
-        <FormProvider {...handleSubmit}>
-          <form onSubmit={handleSubmit(handleAdd)}>
-            <Controller
-              render={({field}) => (
-                <TextField
-                  {...field}
-                  fullWidth
-                  label="Name"
-                  required
-                  onChange={(e) => field.onChange(e)}
-                  value={field.value}
-                  type={"text"}
-                />
-              )}
-              name="name"
-              control={control}
-            />
-            <Controller
-              render={() => (
-                <TextField
-                  select
-                  fullWidth
-                  defaultValue=""
-                  label="Select"
-                  required
-                >
-                  <MenuItem key="1" value="1">
-                    test
-                  </MenuItem>
-                </TextField>
-              )}
-              name="test"
-              control={control}
-            />
-            <DialogActions>
-              <Button onClick={() => handleClose()} color="primary">
-                Cancel
-              </Button>
-              <Button type="submit" color="primary">
-                Add
-              </Button>
-            </DialogActions>
-          </form>
-        </FormProvider>
+        <form onSubmit={handleSubmit(handleAdd)}>
+          <TextField
+            fullWidth
+            label="Name"
+            required
+            type={"text"}
+            inputProps={register('name', {
+              required: 'Please set name',
+            })}
+          />
+          <TextField
+            select
+            fullWidth
+            label="Select"
+            defaultValue=''
+            inputProps={register('owner', {
+              required: 'Please set owner',
+            })}
+          >
+            {persons.map((person, index) => (
+              <MenuItem key={index} value={person}>
+                {person.name}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            select
+            fullWidth
+            label="Select"
+            defaultValue=''
+            inputProps={register('cuisine', {
+              required: 'Please set cuisine',
+            })}
+          >
+            {cuisines.map((cuisine, index) => (
+              <MenuItem key={index} value={cuisine}>
+                {cuisine.name}
+              </MenuItem>
+            ))}
+          </TextField>
+          <DialogActions>
+            <Button onClick={() => handleClose()} color="primary">
+              Cancel
+            </Button>
+            <Button type="submit" color="primary">
+              Add
+            </Button>
+          </DialogActions>
+        </form>
       </DialogContent>
     </Dialog>
   );
